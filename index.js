9241f8a5abcdbc9e3fdeddde0994d44bd615f9d9
R::- <nicolasrfarias@gmail.com>
2016-10-30 01:44:57 -0600

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 4577801366504;
+var nonce = 4677801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -343,7 +343,6 @@ function receivedMessage(event) {
         break;
 
       case 'Si':
-        sendInfoInversion(senderID);
         sendInfoSaldoBitcoin(senderID);
         break;
       case 'No':
