d553c27e824b451ff4034a85edb083b7f46eb67e
R::- <nicolasrfarias@gmail.com>
2016-10-30 04:31:16 -0600

    Version 2.0

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
 var nonce =13077801366505;
-var nonce2 = 12977801366505
+var nonce2 = 13277801366505
 
 var offset = 0;
 var limit = 5;
