4c587b35a63a517c6cdcc830a794173a2505b6f7
R::- <nicolasrfarias@gmail.com>
2016-10-30 00:48:10 -0500

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 2777801366504;
+var nonce = 2877801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -119,7 +119,7 @@ var req = https.request(options, function(res) {
       var body = Buffer.concat(chunks);
       var str = body.toString().split(',"');
 
-      console.log(str);
+      console.log(str[1]);
     });
 
 });
