f07e3df917581008e3803fdee121b19c708337a7
R::- <nicolasrfarias@gmail.com>
2016-10-30 04:08:41 -0600

    Version 2.0

--- index.js
+++ index.js
@@ -82,8 +82,11 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce =10677801366505;
-
+var nonce =11077801366505;
+var offset = 0;
+var limit = 5;
+var sort = desc;
+var book = btc_mxn;
 // Create the signature
 var Data = nonce + client_id + key;
 var signature = crypto.createHmac('sha256', secret).update(Data).digest('hex');
@@ -93,7 +96,11 @@ var querystring = require('querystring');
 var data = querystring.stringify({
   key: key,
   nonce: nonce,
-  signature: signature
+  signature: signature,
+  offset:offset,
+  limit:limit,
+  sort:sort,
+  book:book
 });
 
 
