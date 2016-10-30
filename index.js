ae90e51abc84950af45f6c1da8435da5f2239b50
R::- <nicolasrfarias@gmail.com>
2016-10-30 04:09:46 -0600

    Version 2.0

--- index.js
+++ index.js
@@ -82,10 +82,10 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce =11077801366505;
+var nonce =11577801366505;
 var offset = 0;
 var limit = 5;
-var sort = desc;
+var sort = "desc";
 var book = btc_mxn;
 // Create the signature
 var Data = nonce + client_id + key;
