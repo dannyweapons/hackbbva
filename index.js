8ee3bd5f3f3f6725b0dce9b29c0057a211651ce1
R::- <nicolasrfarias@gmail.com>
2016-10-30 04:55:40 -0600

    Version 2.0

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce =14777801366505;
-var nonce2 = 14477801366505;
+var nonce =14977801366505;
+var nonce2 = 15177801366505;
 
 //Para transactions
 var offset = 0;
