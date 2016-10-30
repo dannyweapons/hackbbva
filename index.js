4fa02e92fca75e1da7eee41ca23ea386f698c033
R::- <nicolasrfarias@gmail.com>
2016-10-30 02:44:35 -0600

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 7377801366504;
+var nonce = 7577801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
