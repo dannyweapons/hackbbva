f77f855fa4cefff9f59d0a556ac30117c8244e72
R::- <nicolasrfarias@gmail.com>
2016-10-30 03:37:24 -0600

    fix

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
 var nonce = 10577801366504;
-var nonce2 = 11077801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -166,7 +165,6 @@ var req = https.request(options, function(res) {
       console.log("Saldo de Bitcoin : ", json.btc_available);
       console.log("Fee. ni idea de que sea : ", json.fee);
       console.log("Saldo de pesos : ", json.mxn_available);
-      Buffer.clear();
     });
 
 });
