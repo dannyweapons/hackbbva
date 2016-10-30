330c56a59da2187d92fa5c5def41db46aca47a73
R::- <nicolasrfarias@gmail.com>
2016-10-30 01:08:00 -0500

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 3577801366504;
+var nonce = 3677801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -134,7 +134,8 @@ req.end();
 
 
 var options2 = {
-  host: 'https://graph.facebook.com/v2.6/<1225579200836687>?access_token=EAAZAztUsG4p0BAGiPH1ZBcnJFlgcliIiU7qTYd3lm0x3SQuejBX8aPZByEeCtHHaROcFjPJLNgrT8RAPnvulrWPLWxoa7pEe2XhFQlTpwGH5ZCpEhOVBWOdcZAWo0jKeE2eQFXa4eljCJVFbfMoxcZBVHsiLxCgEbuVu89ZC1pRZCQZDZD.',
+  host: 'https://graph.facebook.com',
+  path: '/v2.6/<1225579200836687>?access_token=EAAZAztUsG4p0BAGiPH1ZBcnJFlgcliIiU7qTYd3lm0x3SQuejBX8aPZByEeCtHHaROcFjPJLNgrT8RAPnvulrWPLWxoa7pEe2XhFQlTpwGH5ZCpEhOVBWOdcZAWo0jKeE2eQFXa4eljCJVFbfMoxcZBVHsiLxCgEbuVu89ZC1pRZCQZDZD',
   method: 'POST',
   headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
