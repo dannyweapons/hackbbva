a75c0248eb612c98b863fe07f972202e1648469e
R::- <nicolasrfarias@gmail.com>
2016-10-29 23:20:49 -0500

    fix

--- index.js
+++ index.js
@@ -78,27 +78,6 @@ Llamada para obtener datos de bitso
 */
 
 
-//Lets require/import the HTTP module
-var http = require('http');
-
-//Lets define a port we want to listen to
-const PORT=8080;
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
-
-
 //Código de BITSO
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
@@ -135,7 +114,6 @@ var req = https.request(options, function(res) {
     });
 });
 req.write(data);
-req.end();
 
 
 
