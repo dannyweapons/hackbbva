f8f790246341f48139ce4dcff439da149138a87f
R::- <nicolasrfarias@gmail.com>
2016-10-30 01:54:03 -0600

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 4777801366504;
+var nonce = 4877801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -320,6 +320,8 @@ function receivedMessage(event) {
       case 'Si':
         sendInfoInversion(senderID);
         sendInfoSaldoBitcoin(senderID);
+        sendInfoFee(senderID);
+        send
         break;
       case 'No':
         sendInfoBitcoin(senderID);
@@ -945,7 +947,34 @@ function sendInfoSaldoBitcoin(recipientId) {
       id: recipientId
     },
     message: {
-        text: "Tu cuenta saldo es de:" + jsonbitsoc.btc_available + "B"
+        text: "Tu saldo disponible en bitcoins es de :" + jsonbitsoc.btc_available + "."
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendInfoSaldoPesos(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+        text: "Tu saldo disponible en pesos es de:" + jsonbitsoc.mxn_available + "."
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+
+function sendInfoFee(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+        text: "Esta es tu comision por transaccion:" + jsonbitsoc.fee + "mxn."
     }
   };
 
