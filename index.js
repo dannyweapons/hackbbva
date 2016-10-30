e136153fdf174ccb1180c735ffcad10ee3f77b94
R::- <nicolasrfarias@gmail.com>
2016-10-30 02:58:50 -0600

    fix

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ Llamada para obtener datos de bitso
 var secret = "d8d0ac2fd6ba1d4949db0a3dc7a52170";//"BITSO API SECRET";
 var key = "oCFkKHCMfh";//"BITSO API KEY";
 var client_id ="151841";//;"BITSO CLIENT ID";
-var nonce = 7977801366504;
+var nonce = 8177801366504;
 
 // Create the signature
 var Data = nonce + client_id + key;
@@ -452,7 +452,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(payload, "payload called");
 
 
 if (payload=="Balance") {
